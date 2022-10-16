f14190895d52e4480631cc897b7c4935c1b196aa
Lucas Mello22 <lucas.s.mello@outlook.com>
2022-10-16 16:25:12 -0300

    meu primeiro projeto java

---
+++ meu primeiro ptojeto java/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        String primeiroNome = "Lucas";
+        String segundoNome = "José Souza de Mello";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método é " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
